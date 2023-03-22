97c64de4d11f6070f993fc13305704116e6eb1bf
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-22 17:07:13 +0100

    FEAT correct json file vers

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ createApp({
   data() {
     return{
       title: 'ToDo List',
-      todos: [],
+      todos: [], //crei array vuoto
     }
   },
   methods:{ 
-    fetchTodoList(){ //faccio la chiamata al server.php per prendere il json
-
+    fetchTodoList(){ // --3--  faccio la chiamata al server.php per prendere il json FUNZIONE CHE FA CHIAMATA
       axios.get('server.php')
-
-    }
+      .then(res => {   //--5-- Recupero la risposta PS. a questo punto noi abbiamo direttamente l'array, VEDI CONSOLE LOG
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
   },
   mounted(){
-
-  },
+    this.fetchTodoList() // --4-- (invoco la funzione on mounted) PS. vai su"localhost/php-todo-list-json" poi inspector rete>fetch/XHR e vedi che ora viene fatta con successo la chiamata AXIOS al file server.php, che a sua volta prende e converte il file JSON
+  },  
 
 }).mount('#app')
--- server.php
+++ server.php
@@ -8,8 +8,36 @@ $todo_string = file_get_contents('./todo.json');
 //in questo modo con il server leggeremo i dati del json e gli stamperemo proprio come JSON
 header('Content-Type: application/json');
 
-//vedi cosa stai stampando
-echo $todo_string; //VAI SU - localhost/php-todo-list-json/server.php - PER VEDERE COSA SUCCEDE
+//CONVERSIONE IN QUALCOSA DI COMMESTIBILE DA PHP
 
-//DOPO AVER MODIFICATO L'HEADER IN APPLICATION/JSON,
+
+
+//vedi cosa stai stampando
+// echo $todo_string; 
+
+//DOPO AVER MODIFICATO L'HEADER IN APPLICATION/JSON, (STEP 2)
 //POSSIAMO PASSARE AD USARE =AXIOS= PER FARE LA CHIAMATA (vai su app.js per usare Axios)
+
+
+
+
+
+
+
+
+
+
+//SE VOLESSI STRUTTURARE UN ARRAY PIU' ARTICOLATO
+
+// (V1) decodifico, passandogli la stringa con ilJSON INTERNO $todo_string
+$todo_list = json_decode($todo_string,true);
+
+// (V2) questa sara' la struttura che dovra' avere il mio oggetto Che poi RICODIFICHERO'
+$response = [
+  'results' => $todo_list,
+  'success' => true
+];
+
+//STAMPIAMO LA RESPONSE
+// (V3) ricodifico, passandogli response (che e' ora un oggetto)
+echo json_encode($response);
